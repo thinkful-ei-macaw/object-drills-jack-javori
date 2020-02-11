943404fdd06e6edd14e455b79c49d5db8a46f0d2
Jack Lansing <jack.lansing@live.com>
2020-02-11 15:51:12 -0500

    problems 5 and 6

--- object-drills.js
+++ object-drills.js
@@ -31,15 +31,44 @@ const food = {
 
 console.log(food.meals[5]);
 
-function Person(name, jobtitle) {
+function Person(name, jobTitle, boss) {
   this.name = name;
-  this.jobtitle = jobtitle;
+  this.jobTitle = jobTitle;
+  this.boss = boss;
 }
 
-const newArray = [new Person('Dog', 'Police'), new Person('Fish', 'Doctor'),
-new Person('Cat', 'Teacher'), new Person('Squirrel', 'Firefighter'),
-new Person('Rabbit', 'Banker')];
+const newArray = [new Person('Dog', 'Police', 'Squirrel'), new Person('Fish', 'Doctor', 'Squirrel'),
+new Person('Cat', 'Teacher', 'Squirrel'), new Person('Squirrel', 'Firefighter'),
+new Person('Rabbit', 'Banker', 'Squirrel')];
 
-for(let i in newArray) {
-  console.log(newArray[i]);
+// for(let i in newArray) {
+//   console.log(newArray[i]);
+// }
+
+newArray.forEach(employee => {
+  employee.boss ? 
+  console.log(`${employee.jobTitle} ${employee.name} reports to ${employee.boss}`) :
+  console.log(`${employee.jobTitle} ${employee.name} doesn't report to anybody`);
+});
+
+function decode(string) {
+    
+  const cipher = {
+    a: 2,
+    b: 3,
+    c: 4,
+    d: 5,
+  };
+
+  return cipher[string[0]] ? string.charAt(cipher[string[0]]-1) : ' ';
+
 }
+
+function decodeWords(words) {
+  return words.split(' ').map(e => decode(e)).join('');
+}
+
+let encrypted = 'craft block argon meter bells brown croon droop';
+
+console.log(decodeWords(encrypted));
+
